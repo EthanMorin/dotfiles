a75b22b69977f0b9c362cf12c35edb0aab3224f0
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-04-29 14:13:30 -0600

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+yay -S eza zoxide fish starship visual-studio-code-bin jetbrains-toolbox vim kitty neovim lazygit lazydocker-bin 
+
 git pull
 stow --adopt .
 git restore .
 
-#yay -S 
